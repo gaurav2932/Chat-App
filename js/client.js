951319aa795fdc2b68d81a843a5b8c5e40f6590a
Gaurav Gupta <guptaguitar2020@gmail.com>
2021-12-18 14:41:49 +0530

    Initial commit

---
+++ js/client.js
@@ -0,0 +1,43 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageinp')
+const messageContainer = document.querySelector(".container")
+var audio = new Audio('tum.m4a');
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+    audio.play();
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`you: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+})
+window.onscroll = function() {scrollFunction()};
+
+
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name =>{
+    append(`${name} joined the chat`, 'right')
+})
+
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('left', data =>{
+    append(`${name} left the chat`, 'left')
+})
+  
